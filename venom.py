d3dfc22d249699f26f8954d953fd74f9c1da19ab
shyam20051 <shyamddos@gmail.com>
2024-07-31 09:50:20 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '6812992138:AAEJTcDYpcUfAqL28IGBmnXh0HeCipxfOqg'
+ADMIN_ID = '5816432312'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
